db02c4e337f061a96d8ddd8d59a5e06760d177d1
Diego Rodrigues <diegorodrigues.developer@gmail.com>
2021-01-20 13:09:10 -0300

    Criação de Person

---
+++ src/Presentation/Controllers/PersonController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace curso_RestApi1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController : Controller
+    {
+        private readonly ILogger<PersonController> _logger;
+        public PersonController(ILogger logger)
+        {
+            _looger = logger;
+        }
+    }
+}
